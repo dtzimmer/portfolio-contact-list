4816ba91a5a33285219b4513d3621d6db6baa19d
Unknown <daniel.zimmerman@gmail.com>
2018-08-07 12:54:25 -0600

    Put contact form on the website

--- src/ListofContacts.js
+++ src/ListofContacts.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react'
 import './App.css';
 import Header from "./Header";
 import Footer from "./Footer";
+import Contactform from "./ContactForm"
 
 class ListOfContacts extends Component {
     constructor() {
@@ -13,7 +14,7 @@ class ListOfContacts extends Component {
 
     handleSubmit = async (_id) => {
 
-        await fetch('http://localhost:4000' + '/' + _id, {
+        await fetch('http://localhost:4000/' + _id, {
             method: 'delete'
         }).then(response => {
             response.json().then(json => {
@@ -36,6 +37,19 @@ class ListOfContacts extends Component {
 
     }
 
+    formAppear = () => {
+
+    }
+
+    // hiddenForm() {
+    //     var x = document.getElementById("myDIV");
+    //     if (x.style.display === "none") {
+    //         x.style.display = "block";
+    //     } else {
+    //         x.style.display = "none";
+    //     }
+    // }
+
 
     async componentWillMount() {
 
@@ -48,7 +62,8 @@ class ListOfContacts extends Component {
                 <Header/>
                 <h2>Fetching Contacts</h2>
                 <div>
-                    <h3>Coming Soon</h3>
+                    <h3>Coming Soon...</h3>
+                <Contactform/>
                     <div>
                         {this.state.contacts.map(contact =>
                         <div className="border" key={contact._id}>
@@ -56,6 +71,12 @@ class ListOfContacts extends Component {
                             <p> Phone: {contact.phone}</p>
                             <p> Email: {contact.email}</p>
                             <button onClick={() => this.handleSubmit(contact._id)}>Delete</button>
+                            {/*<button onClick={() => this.hiddenForm(contact._id)}>Click Me</button>*/}
+                            {/*<div id="myDIV" key={contact._id}>*/}
+                                {/*<label form="firstname">First Name</label>*/}
+                                {/*<input type="text" id="firstname" name="firstname"*/}
+                                       {/*placeholder="First name" onChange={e => this.setState({ firstName: e.target.value})}/>*/}
+                            {/*</div>*/}
                         </div>
                     )}
                     </div>
